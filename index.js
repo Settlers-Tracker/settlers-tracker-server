4257d5007768470c7342c6eba8c37eef4bb1d049
Pete Givens <pgivens@gmail.com>
2018-03-01 19:57:26 -0600

    setup basic express server

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const http = require("http");
+const path = require("path");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.get("/hello", (req, res) => res.send("Hello World!"));
+
+const port = process.env.PORT || 3000;
+
+const server = http.createServer(app);
+
+server.listen(port, () => console.log(`listening on port ${port}`));
